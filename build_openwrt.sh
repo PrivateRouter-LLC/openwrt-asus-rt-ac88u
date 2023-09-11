51cb7ce3a501d47914a81b0066d4236cb28ae534
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-09-10 20:02:18 -0400

    Update build_openwrt.sh

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT="$(pwd)/images"
-BUILD_VERSION="22.03.5"
+BUILD_VERSION="21.02.3"
 BOARD_NAME="bcm53xx"
 BOARD_SUBNAME="generic"
 BUILDER="https://downloads.openwrt.org/releases/${BUILD_VERSION}/targets/${BOARD_NAME}/${BOARD_SUBNAME}/openwrt-imagebuilder-${BUILD_VERSION}-${BOARD_NAME}-${BOARD_SUBNAME}.Linux-x86_64.tar.xz"
